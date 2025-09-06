b2e4072ff1660d7181139dee1613c3a831252fa1
Kaique Silva <kaique.guerra013@hotmail.com>
2025-09-06 13:14:41 -0300

    ajustando pag

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://yane-moda-bags.vercel.app"  
+}));
+
 app.use(express.json());
 
 
@@ -23,3 +26,5 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+
