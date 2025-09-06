a91c8fcf13efe6f07de0ce1081f76a695d0f0e1e
Kaique Silva <kaique.guerra013@hotmail.com>
2025-09-06 10:52:51 -0300

    mercado pago

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const routesCheckout = require("./routes/routesCheckout");
-app.use("/api/checkout", routesCheckout);
 
 
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Conectado ao MongoDB"))
-  .catch(err => console.error("❌ Erro de conexão:", err));
+const routesCheckout = require("./routes/routesCheckout");
+const produtoRoutes = require("./routes/routesProdutos");
+
+app.use("/api/checkout", routesCheckout);
+app.use("/api/produtos", produtoRoutes);
 
 
 app.get("/", (req, res) => {
@@ -22,13 +23,10 @@ app.get("/", (req, res) => {
 });
 
 
-const produtoRoutes = require("./routes/routesProdutos");
-app.use("/api/produtos", produtoRoutes);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Conectado ao MongoDB"))
+  .catch(err => console.error("❌ Erro de conexão:", err));
 
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
