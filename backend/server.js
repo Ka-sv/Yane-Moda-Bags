5a6576ec0958dc1cda147ac4857e4215d5cf0deb
Kaique Silva <kaique.guerra013@hotmail.com>
2025-09-15 16:44:29 -0300

    ajuste no cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app = express();
+const app = express(); // 🔹 app precisa vir antes do app.use
+
+// Configuração de CORS
+const allowedOrigins = [
+  "https://yane-moda-bags.vercel.app",
+  "http://127.0.0.1:5500",
+  "http://localhost:5500"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 
 app.use(express.json());
 
@@ -18,7 +35,6 @@ app.use("/api/checkout", checkoutRoutes);
 // Webhook do Mercado Pago
 const webhookRoutesMp = require("./routes/routesWebhookMp");
 app.use("/api/mp/webhook", webhookRoutesMp);
-
 
 app.get("/orders/:id/status", async (req, res) => {
   try {
@@ -36,19 +52,3 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-const allowedOrigins = [
-  "https://yane-moda-bags.vercel.app", 
-  "http://127.0.0.1:5500",             
-  "http://localhost:5500"              
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}));
